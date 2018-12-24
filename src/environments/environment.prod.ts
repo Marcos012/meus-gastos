69e43e9eca502300104dd07a9e6f469f687d44ea
marcos012 <marcospaulo.ad@gmail.com>
2018-12-24 19:48:04 -0200

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyDpVxGWzdXfHc8GkOJMzUFngi0I_7GrjBw',
+    authDomain: 'daily-gastos-angular.firebaseapp.com',
+    databaseURL: 'https://daily-gastos-angular.firebaseio.com',
+    projectId: 'daily-gastos-angular',
+    storageBucket: 'daily-gastos-angular.appspot.com',
+    messagingSenderId: '247371946160'
+  }
 };
